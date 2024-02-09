320355529242157fea8e692ea367fc8ff4d10048
Kris Kurniawan <kris7kurniawan@gmail.com>
2024-02-09 17:31:23 +0900

    fixing: fix brands

--- resources/views/content/brand.blade.php
+++ resources/views/content/brand.blade.php
@@ -1,58 +1,63 @@
-<?php
+@extends('layouts.app')
 
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                @if (session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
 
-
-namespace App\Http\Controllers;
-
-
-
-use App\Models\Brand;
-
-use Illuminate\Http\Request;
-
-
-
-class BrandController extends Controller
-
-{
-
-    public function index()
-
-    {
-
-        $brands = Brand::all();
-
-        return view('brand.blade.php', compact('brands'));
-
-    }
-
-
-
-    public function create()
-
-    {
-
-        return view('brand.blade.php');
-
-    }
-
-
-
-    public function store(Request $request)
-
-    {
-
-        $request->validate([
-
-            'name' => 'required|unique:brands,name'
-
-        ]);
-
-
-
-        Brand::create($request->all());
-
-
-
-        return redirect()->route('brands.index')->with('success', 'Brand created successfully.');
-
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        There were some errors with your request.
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title">Brand</h3>
+                            <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#tambah-pengguna">Tambah Brand</button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th width="7%">No</th>
+                                    <th>Nama</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $i = 1;
+                                @endphp
+                                @foreach ($brands as $brand)
+                                    <tr>
+                                        <td>{{ $i++ }}</td>
+                                        <td>{{ $brand->nama }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+                        <ul class="pagination pagination-sm m-0 float-right">
+                            {{ $brands->links() }}
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+    </div>
+@endsection
