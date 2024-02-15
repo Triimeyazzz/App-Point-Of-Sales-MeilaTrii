34a46b12cb80f0d489ef0340276d23e834d5f7c8
Triimeyazzz <trimei.astuti06@gmail.com>
2024-02-15 18:48:53 +0700

    changes: update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="../../index2.html" class="h2"><b>Admin<br>MeilaaTrimeyy<br></b>Point Of Sales</a>
+                <a href="../../index2.html" class="h2"><b>Login<br>MeilaaTrimeyy<br></b>Point Of Sales</a>
             </div>
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
